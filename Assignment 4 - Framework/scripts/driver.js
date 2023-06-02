b39adf1ed9942e1a508bf97b430db2c03ab82f0d
Joshua <bubbahamby@gmail.com>
2023-06-01 18:02:22 -0600

    update

--- Assignment 4 - Framework/scripts/driver.js
+++ Assignment 4 - Framework/scripts/driver.js
@@ -5,6 +5,28 @@ MySample.main = (function() {
     let canvas = document.getElementById('canvas-main');
     let gl = canvas.getContext('webgl2');
 
+
+    let vertices = new Float32Array([
+        0.0, 0.5, 0.0,
+        0.5, 0.0, 0.0,
+        -0.5, 0.0, 0.0
+    ]);
+    let vertexColors = new Float32Array([
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0
+    ]);
+    let indices = new Uint16Array([0, 1, 2]);
+    let vertexBuffer = gl.createrBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER,null);
+
+    
+    
+
+
+
     //------------------------------------------------------------------
     //
     // Scene updates go here.
