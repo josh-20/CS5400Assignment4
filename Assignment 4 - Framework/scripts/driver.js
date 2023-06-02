0d10ba4411f247588434280be457208fec901980
Joshua <bubbahamby@gmail.com>
2023-06-01 18:46:00 -0600

    update

--- Assignment 4 - Framework/scripts/driver.js
+++ Assignment 4 - Framework/scripts/driver.js
@@ -17,10 +17,26 @@ MySample.main = (function() {
         0.0, 0.0, 1.0
     ]);
     let indices = new Uint16Array([0, 1, 2]);
+    // Prepare vertex buffer
     let vertexBuffer = gl.createrBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER,null);
+
+    // Prepare Color Buffer
+    let vertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER,vertexColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,vertexColors,gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER,null);
+    
+    // Prepare Index Buffer
+    let indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,indices,gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,null);
+
+
+    
 
     
     
--- Assignment 4 - Framework/shaders/simple.vert
+++ Assignment 4 - Framework/shaders/simple.vert
@@ -1,1 +1,10 @@
-#version 300 es
+#version 300 es 
+in vec4 aPostion;
+in vecr aColor;
+out vec4 vColor;
+
+
+void main(){
+    gl_Position = aPostion;
+    vColor = aColor;
+}
