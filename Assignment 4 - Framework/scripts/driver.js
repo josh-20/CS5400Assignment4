f81465fd1a149657f3e243c349791e82fff52c33
Josh Hamby <bubbahamby@gmail.com>
2023-06-03 08:25:36 -0600

    new framework

--- Assignment 4 - Framework/scripts/driver.js
+++ Assignment 4 - Framework/scripts/driver.js
@@ -5,46 +5,6 @@ MySample.main = (function() {
     let canvas = document.getElementById('canvas-main');
     let gl = canvas.getContext('webgl2');
 
-
-
-    let vertices = new Float32Array([
-        0.0, 0.5, 0.0,
-        0.5, 0.0, 0.0,
-        -0.5, 0.0, 0.0
-    ]);
-    let vertexColors = new Float32Array([
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0
-    ]);
-    let indices = new Uint16Array([0, 1, 2]);
-    // Prepare vertex buffer
-    let vertexBuffer = gl.createrBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER,null);
-
-    // Prepare Color Buffer
-    let vertexColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER,vertexColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,vertexColors,gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER,null);
-    
-    // Prepare Index Buffer
-    let indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,indices,gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,null);
-    console.log("hello");
-
-
-
-
-    
-    
-
-
-
     //------------------------------------------------------------------
     //
     // Scene updates go here.
