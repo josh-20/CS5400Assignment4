b1421b5ca2ebff6a33ba558f10d9d2576fa04836
Josh Hamby <bubbahamby@gmail.com>
2023-06-04 21:44:27 -0600

    update

--- Assignment 4 - Framework/scripts/driver.js
+++ Assignment 4 - Framework/scripts/driver.js
@@ -5,29 +5,29 @@ MySample.main = (function() {
     let canvas = document.getElementById('canvas-main');
     let gl = canvas.getContext('webgl2');
 
-    let object = {
+    let objectCube = {
         vertices: new Float32Array([]),
         vertexColors: new Float32Array([]),
-        center: new Float32Array([0,0,-1]),
+        center: new Float32Array([0,0,-2]),
         parallel: new Float32Array([]),
         perspective: new Float32Array([]),
 
     };
-    object.vertices = new Float32Array([
+    objectCube.vertices = new Float32Array([
         0.5, 0.5, 0.0, 1.0,
         -0.5, 0.5, 0.0, 1.0,
         -0.5, -0.5, 0.0, 1.0,
         -0.5, -0.5, 0.0, 1.0,
         0.5, -0.5, 0.0, 1.0,
-        0.5, 0.5, 0.0, 1.0,
-        0.5, 0.5, 1.0, 1.0,
-        -0.5, 0.5, 1.0, 1.0,
-        -0.5, -0.5, 1.0, 1.0,
-        -0.5, -0.5, 1.0, 1.0,
-        0.5, -0.5, 1.0, 1.0,
-        0.5, 0.5, 1.0, 1.0,    
+        0.5, 0.5, 0.0, 1.0, // End of Front ace
+        // 0.5, 0.5, 1.0, 1.0,
+        // -0.5, 0.5, 1.0, 1.0,
+        // -0.5, -0.5, 1.0, 1.0,
+        // -0.5, -0.5, 1.0, 1.0,
+        // 0.5, -0.5, 1.0, 1.0,
+        // 0.5, 0.5, 1.0, 1.0, // End of Back Face
     ]);
-    object.vertexColors = new Float32Array([
+    objectCube.vertexColors = new Float32Array([
         1.0, 0.0, 0.0,
         0.0, 1.0, 0.0,
         0.0, 0.0, 1.0,
@@ -36,23 +36,23 @@ MySample.main = (function() {
         1.0, 0.0, 0.0
         
     ]);
-    object.parallel = new Float32Array([
-        2/(object.vertices[0]-object.vertices[4]), 0, 0, -((object.vertices[4]+object.vertices[0])/(object.vertices[0] - object.vertices[4])),
-        0, 2/(object.vertexColors[1]-object.vertices[9]), 0, -((object.vertices[1]+object.vertices[9])/(object.vertices[1] - object.vertices[9])),
-        0, 0, 2/(object.vertices[26] - object.vertices[2]), -((object.vertices[26]+object.vertices[0])/(object.vertices[26] - object.vertices[0])),
+    objectCube.parallel = new Float32Array([
+        2/(objectCube.vertices[0]-objectCube.vertices[4]), 0, 0, -((objectCube.vertices[4]+objectCube.vertices[0])/(objectCube.vertices[0] - objectCube.vertices[4])),
+        0, 2/(objectCube.vertexColors[1]-objectCube.vertices[9]), 0, -((objectCube.vertices[1]+objectCube.vertices[9])/(objectCube.vertices[1] - objectCube.vertices[9])),
+        0, 0, 2/(objectCube.vertices[26] - objectCube.vertices[2]), -((objectCube.vertices[26]+objectCube.vertices[0])/(objectCube.vertices[26] - objectCube.vertices[0])),
         0, 0, 0, 1
     ]);
     let indices = new Uint16Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
     // Prepare vertex buffer
     let vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,object.vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER,objectCube.vertices, gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER,null);
 
     // Prepare Color Buffer
     let vertexColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER,vertexColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,object.vertexColors,gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER,objectCube.vertexColors,gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER,null);
     
     // Prepare Index Buffer
@@ -63,6 +63,7 @@ MySample.main = (function() {
 
     // Prepare Vertex Shader
     let vertexShaderSource = `#version 300 es
+    uniform mat4 uParallel
     in vec4 aPosition;
     in vec4 aColor;
     out vec4 vColor;
@@ -71,11 +72,12 @@ MySample.main = (function() {
     gl_Position = aPosition;
     vColor = aColor;
     }`;
+    
     let vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader,vertexShaderSource);
     gl.compileShader(vertexShader);
     console.log(gl.getShaderInfoLog(vertexShader));
-
+    
     // Prepare Fragment Shader
     let fragmentShaderSource = `#version 300 es
     precision lowp float;
@@ -83,30 +85,44 @@ MySample.main = (function() {
     out vec4 outColor;
     void main()
     {
-    outColor = vColor;
+        outColor = vColor;
     }`
     let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderSource);
     gl.compileShader(fragmentShader);
-
+    
     // Shader Program
     let shaderProgram = gl.createProgram();
+    let location = gl.getUniformLocation(shaderProgram, 'uParallel');
+    gl.uniformMatrix4fv(location,false,transposeMatrix4x4(objectCube.parallel));
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
-
-    // Specify Shader & Buffer Object Attributes
+    
+    // Specify Shader & Buffer objectCube Attributes
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     let position = gl.getAttribLocation(shaderProgram, 'aPosition');
     gl.enableVertexAttribArray(position);
-    gl.vertexAttribPointer(position, 4, gl.FLOAT, false, object.vertices.BYTES_PER_ELEMENT * 4, 0);
+    gl.vertexAttribPointer(position, 4, gl.FLOAT, false, objectCube.vertices.BYTES_PER_ELEMENT * 4, 0);
 
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
     let color = gl.getAttribLocation(shaderProgram, 'aColor');
     gl.enableVertexAttribArray(color);
-    gl.vertexAttribPointer(color, 3, gl.FLOAT, false, object.vertexColors.BYTES_PER_ELEMENT * 3, 0);
+    gl.vertexAttribPointer(color, 3, gl.FLOAT, false, objectCube.vertexColors.BYTES_PER_ELEMENT * 3, 0);
+
+
+    function transposeMatrix4x4(m) {
+        let t = [
+        m[0], m[4], m[8], m[12],
+        m[1], m[5], m[9], m[13],
+        m[2], m[6], m[10], m[14],
+        m[3], m[7], m[11], m[15]
+        ];
+        return t;
+    }
+        
 
 
     // Request Animation Frame
