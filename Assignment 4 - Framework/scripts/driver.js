1a8c26fa1ad3d899bd09e37e30074d276a40ede3
Josh Hamby <bubbahamby@gmail.com>
2023-06-05 15:38:23 -0600

    update

--- Assignment 4 - Framework/scripts/driver.js
+++ Assignment 4 - Framework/scripts/driver.js
@@ -134,7 +134,6 @@ MySample.main = (function() {
     ]);
     
 
-    objectCube.m
     let combinedMatrix = multiplyMatrix4x4(objectCube.parallel, mCameraView);
     let indices = new Uint16Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]);
     // Prepare vertex buffer
